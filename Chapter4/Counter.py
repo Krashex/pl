ef70eae2e040ce62c51b716ec211d26c7ab6b356
Oleg Stepanov <krash.ex@gmail.com>
2017-06-25 13:09:20 +0400

    #PL-5 test

--- Chapter4/Counter.py
+++ Chapter4/Counter.py
@@ -1,7 +1,8 @@
 # Считает за пользователя
 start = int(input('Enter start of counting '))
-end = int(input("Enter end of counting "))
-interval = (int(input("Enter interval ")))
+end = int(input("Enter end of counting 1"))
+interval = (int(input("Enter interval 1")))
+
 if start > end:
     if interval > start - end:
         print("IT'S IMPOSSIBLE")
