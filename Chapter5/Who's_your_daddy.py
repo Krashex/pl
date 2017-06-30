95ab86945a4dee78df3dae4b4872f9222d07e798
Petr <stone.ptr@gmail.com>
2017-06-30 22:45:45 +0400

    refactoring

--- Chapter5/Who's_your_daddy.py
+++
@@ -1,8 +0,0 @@
-Dad = ['Petr', 'Denis']
-Son = ['Oleg', 'Artur']
-Pairs = ["Petr" == "Oleg", "Denis" "Artur"]
-ans = input("Enter Son's name ")
-for ans in Son:
-    for
-    print(Dad)
-input('Press enter')
