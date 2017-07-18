1284ab336592240ea6138fa8b4a0a63fdd8ada23
Oleg Stepanov <krash.ex@gmail.com>
2017-07-18 12:04:08 +0400

    New task

--- chapetr3/task2.py
+++ chapetr3/task2.py
@@ -1,17 +1,43 @@
 import random
 
-COUNT_TRY = 10
+A = None
+MAX = 4
 
-guess = 0
-x = random.randint(1, 2)
-ans = -1
 
-while guess <COUNT_TRY or ans != x:
-    ans = int(input('Gues '))
-    if ans != x:
-        guess += 1
-else:
-    print("you lost")
+def number():
+    x = None
+    x = random.randint(1, MAX)
+    return x
 
-print('You won')
-input('нажмите Enter')
+
+def ask_number():
+    response = None
+    while response not in range(1, MAX, ):
+        response = int(input('Gues '))
+        if response not in range(1, MAX):
+            print('Incorrect number')
+
+    return response
+
+
+def checking():
+    COUNT_TRY = 10
+    GUESSES = 0
+    while GUESSES < COUNT_TRY:
+        correct = number()
+        A = ask_number()
+        if A != correct:
+            GUESSES += 1
+        elif A == correct:
+            print('You won')
+            break
+    if GUESSES == COUNT_TRY:
+        print("you lost")
+
+
+def main():
+    checking()
+    input('нажмите Enter')
+
+
+main()
